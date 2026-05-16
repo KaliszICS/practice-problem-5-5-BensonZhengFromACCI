c9b09fb8a108d2c79ddd452040b7ad5a147696d8
BensonZhengFromACCI <343804126@tdsb.ca>
2026-05-16 15:01:58 +0000

    thanks

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,24 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
+	public static boolean isPalindrome(String word) {
+		word = word.replace(" ", "").toLowerCase();
+		int mid = word.length() / 2;
+		String firstHalf;
+		String secondHalf;
+		if (word.length() % 2 == 0) {
+			// Even length
+			firstHalf = word.substring(0, mid);
+			secondHalf = word.substring(mid);
+		} 
+		else {
+			firstHalf = word.substring(0, mid);
+			secondHalf = word.substring(mid + 1);
+		}
+		String Second = "";
+		for (int start = secondHalf.length() - 1; start >= 0; start--) {
+			Second += secondHalf.charAt(start);
+		}
+		return firstHalf.equals(Second);
+	}
 }
